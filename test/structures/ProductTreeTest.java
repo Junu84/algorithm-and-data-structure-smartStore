4b17e121eb9e2f5359e63103665e14a943496313
junu84 <junu.rahman@stud.hcw.ac.at>
2026-06-18 13:28:00 +0200

    complete M1 ProductTree implementation and JUnit5 verification tests

---
+++ test/structures/ProductTreeTest.java
@@ -0,0 +1,17 @@
+package structures;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTreeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void insert() {
+    }
+}
